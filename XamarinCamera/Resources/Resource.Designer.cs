e9b98f2757908e90e3cc3cb0f416879625e9972a
Kajan <lkajan@hotmail.com>
2016-10-08 00:47:44 +0100

    commit

--- XamarinCamera/Resources/Resource.Designer.cs
+++ XamarinCamera/Resources/Resource.Designer.cs
@@ -57,6 +57,25 @@ namespace XamarinCamera
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050001
+			public const int ImgTakeimg = 2131034113;
+			
+			// aapt resource value: 0x7f050000
+			public const int btntakepicture = 2131034112;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
@@ -81,6 +100,12 @@ namespace XamarinCamera
 			
 			// aapt resource value: 0x7f040000
 			public const int Hello = 2130968576;
+			
+			// aapt resource value: 0x7f040003
+			public const int app_name = 2130968579;
+			
+			// aapt resource value: 0x7f040002
+			public const int openCamera = 2130968578;
 			
 			static String()
 			{
