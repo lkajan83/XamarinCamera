8f665120733cc06e1289856a5eb13a8d783b16c5
Kajan <lkajan@hotmail.com>
2016-10-07 22:03:07 +0100

    Add project files.

---
+++ XamarinCamera/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace XamarinCamera
+{
+    [Activity(Label = "XamarinCamera", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
